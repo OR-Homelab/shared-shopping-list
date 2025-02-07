5ae364d3026f425cf89873e7215aa3cb1700d494
Oliver Dalgaard <oliver@ihd.dk>
2025-02-07 18:09:21 +0100

    Added rate-limiting

--- server.js
+++ server.js
@@ -8,11 +8,31 @@ const User = require(path.join(__dirname, '/src/models/users.models'));
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+
+// Sets a maximum of 10000 requests per ip for a 15 minute window.
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 8000,
+    handler: () => console.log("User being rate limited")
+});
+
+// Slow down request rate after 100 requests by the amount of requests times 200ms within a 15 minute window.
+const speedLimiter = slowDown({
+    windowMs: 15 * 60 * 1000,
+    delayAfter: 10,
+    delayMs: (hits) => hits * 100, // Slows down for hits * 100 ms.
+});
 
 const appRouter = require(path.join(__dirname, 'src/routes/router'));
 const app = express();
 
 const port = process.env.PORT;
+
+// Rate limiting:
+app.use(limiter);
+app.use(speedLimiter);
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
